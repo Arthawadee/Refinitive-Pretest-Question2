09cd2adcbfff02fbf68b6e4d55eda5e3e43a33f9
unknown <por_25418@hotmail.com>
2021-08-27 14:31:16 +0700

    create filter function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [inputText, setInputText] = useState("");
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("https://api.publicapis.org/categories")
+        .then((res) => res.json())
+        .then((result) => {
+          console.log("result = ", result);
+          setData(result);
+        });
+    };
+    fetchData();
+  }, []);
+
+  const filterArray = () => {
+    if (inputText.length === 0) {
+      return data;
+    } else {
+      return data.filter((i) => {
+        return i.toLowerCase().includes(inputText.toLowerCase());
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        className="input"
+        type="text"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Array</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filterArray().map((i) => {
+            return (
+              <tr>
+                <td>{i}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
